5dda6a69aeacf3ae3afaf090e9a309d182e5f5a0
David Sapiro <dsap48@gmail.com>
2024-04-02 17:06:17 -0700

    hidden arrows when needed

--- src/Carousel.test.jsx
+++ src/Carousel.test.jsx
@@ -31,7 +31,7 @@ it("works when you click on the right arrow", function () {
     container.querySelector('img[alt="testing image 2"]')
   ).toBeInTheDocument();
 });
-
+//TODO: click right arrow twice in case nothing happens with arrows
 it("works when you click on the left arrow", function () {
   const { container } = render(
     <Carousel
@@ -63,6 +63,50 @@ it("works when you click on the left arrow", function () {
   ).toBeInTheDocument();
 });
 
+it("left arrow does not exist with image 1", function () {
+  const { container } = render(
+    <Carousel
+      photos={TEST_IMAGES}
+      title="images for testing"
+    />
+  );
+  // expect the first image to show, but not the second
+  expect(
+    container.querySelector('img[alt="testing image 1"]')
+  ).toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="testing image 2"]')
+  ).not.toBeInTheDocument();
+
+  //TODO: toBeFalsey is a bit nebulus. Test for the actual value
+  // expect left arrow not to show
+  const leftArrow = container.querySelector(".bi-arrow-left-circle");
+  expect(leftArrow).toBeFalsy();
+});
+
+it("right arrow does not exist with last image", function () {
+  const { container } = render(
+    <Carousel
+      photos={TEST_IMAGES}
+      title="images for testing"
+    />
+  );
+  // expect the first image to show, but not the second
+  expect(
+    container.querySelector('img[alt="testing image 1"]')
+  ).toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="testing image 2"]')
+  ).not.toBeInTheDocument();
+
+  const rightArrow = container.querySelector(".bi-arrow-right-circle");
+  fireEvent.click(rightArrow);
+  fireEvent.click(rightArrow);
+
+  // expect left arrow not to show
+  expect(rightArrow).toBeFalsy;
+});
+
 it("renders without crashing", function () {
   render(<Carousel photos='hello' title='hello' />);
 });
@@ -71,6 +115,5 @@ it("matches snapshot", function () {
   const { container } = render(<Carousel photos='hello' title='hello' />);
   expect(container).toMatchSnapshot();
 });
-
 
 
